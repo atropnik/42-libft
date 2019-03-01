60b28f399506f952cc6ab2aad949e8c0e2ed5a54
atropnik <atropnikova@gmail.com>
2019-03-01 02:37:13 -0800

    cleaner atoi

---
+++ ft_atoi.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atropnik <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/01 02:09:40 by atropnik          #+#    #+#             */
+/*   Updated: 2019/03/01 02:35:52 by atropnik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_atoi(const char *str)
+{
+	char 	sign;
+	int		total;
+
+	total = 0;
+	sign = 1;
+	while (*str != '\0')
+	{
+		if (*str == '-')
+			sign = -sign;
+		if (*str >= '0' && *str <= '9')
+			total = total * 10 + (*str - 48);
+		str++;
+	}
+	return (sign * total);
+}
+
+// to test
+
+#include <stdio.h>
+
+int	main()
+{
+	char str[] = "    \n\n\v\f\r\t -5234AAAgreghrsth";
+	printf("%d\n", ft_atoi(str));
+	return (0);
+}
