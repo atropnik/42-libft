874eecbd757d2426f3a02626bdf2a5c22b65f1f4
Anna Tropnikova <anna@streetography.com>
2019-03-18 23:50:07 -0700

    new prototypes incoming

--- libft.h
+++ libft.h
@@ -17,6 +17,19 @@
 #include <unistd.h>
 #include <string.h>
 
+typedef struct	s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
+
+t_list	*ft_lstnew(void const *content, size_t content_size);
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t);
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void	ft_lstadd(t_list, **alst, t_list *new);
+void	ft_lstiter(t_list, *lst, void (*f)(t_list *elem));
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
